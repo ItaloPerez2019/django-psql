5b5a6f92ae341f40c47af16054cab7d4f8ea93f9
Italo Perez <pyitalo@gmail.com>
2020-02-19 17:40:07 -0500

    Database was changed.

---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/italoperez/Desktop/sample-django/django-psql/myenv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- producthunt/settings.py
+++ producthunt/settings.py
@@ -75,12 +75,17 @@ WSGI_APPLICATION = 'producthunt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {	
+	'default': {
+	'ENGINE': 'django.db.backends.postgresql',
+	'NAME': 'devtesting',
+	'USER':'dev',
+	'PASSWORD':'g00147441',
+	'HOST':'localhost',
+	'PORT':'5432',
+	}
+	}
+
 
 
 # Password validation
