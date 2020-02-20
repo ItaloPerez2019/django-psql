c53acc256f73ea6f02edb5ea158824e2dbedb5be
Italo Perez <pyitalo@gmail.com>
2020-02-19 22:46:26 -0500

    Log in page created

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
+def signup(request):
+    if request.method == 'POST':
+        # User has info and wants an account now!
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error':'Username has already been taken'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                auth.login(request,user)
+                return redirect('home')
+        else:
+            return render(request, 'accounts/signup.html', {'error':'Passwords must match'})
+    else:
+        # User wants to enter info
+        return render(request, 'accounts/signup.html')
 # Create your views here.
+
+
+
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+
+# def logout(request):
+#     # TODOs need to re-route to the home page
+# # do not forget to log out 
+#     return render(request, 'accounts/logout.html')
